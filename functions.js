2ce4594b58c5b7362d50a74ae5e804527fa49cf4
imoliviant <112634843+imoliviant@users.noreply.github.com>
2022-10-23 22:24:09 -0700

    commit

---
+++ functions.js
@@ -0,0 +1,27 @@
+// mint scribbles
+function mintScribbles() {
+    var amount1 = $("#amount1").val();
+    var content = "Sending transaction from: ";
+    content += account;
+    $("#scrib1").html(content);
+    var event = contractScribble.methods.batchMint(account, amount1).send({ from: account, value: 25000000000000000000 * amount1 })
+        .then(function (receipt) {
+            console.log(receipt);
+    var content = "Transaction sent!: ";
+            //alert("Done!");
+    content += JSON.stringify(receipt.transactionHash);
+    $("#scrib1").html(content);
+        });;
+};
+
+// minted scribbles
+function supplyMinted() {
+    var content = "";
+    var event = contractScribble.methods.totalSupply().call({ from: account })
+        .then(function (result) {
+            console.log(result);
+    var content = "Already minted: ";
+    content += result;
+    $("#scrib2").html(content);
+    });
+};
